6f145a9fe24b6823a8042dabed70f33bb89c56c1
arthxx-123 <arthxx1411@gmail.com>
2025-07-03 10:06:55 +0530

    arraymark.java

--- src/com/vetias/java/arraymark.java
+++ src/com/vetias/java/arraymark.java
@@ -2,7 +2,10 @@ package com.vetias.java;
 
 public class arraymark {
     public static void main(String[] args) {
-        int []marks={89,99}
+        int []marks={89,99,98,97,90};
+        for(int mark:marks){
+            System.out.println(mark);
+        }
     }
     
 }

9faa11566eb14788753b96c9f45673bf4cd8634c
arthxx-123 <arthxx1411@gmail.com>
2025-07-03 15:00:02 +0530

    Added sub class

---
+++ src/com/vetias/java/workshop/temperaturedata/java/beans/Acadamic.java
@@ -0,0 +1,6 @@
+package com.vetias.java.workshop.temperaturedata.java.beans;
+
+public class Acadamic  extends Buildings {
+    private boolean islaibrary Available;
+    private boolean isauditorium Available;
+}
---
+++ src/com/vetias/java/workshop/temperaturedata/java/beans/Hostel.java
@@ -0,0 +1,7 @@
+package com.vetias.java.workshop.temperaturedata.java.beans;
+
+public class Hostel extends Buildings {
+    private boolean ismessAvailable;
+    private int numberofRooms;
+
+}

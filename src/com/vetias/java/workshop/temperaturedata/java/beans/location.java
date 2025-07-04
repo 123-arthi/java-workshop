4307892bafcf05196958993c951123f395ee7ba9
arthxx-123 <arthxx1411@gmail.com>
2025-07-04 14:22:33 +0530

    location.java

--- src/com/vetias/java/workshop/temperaturedata/java/beans/location.java
+++ src/com/vetias/java/workshop/temperaturedata/java/beans/location.java
@@ -4,9 +4,7 @@ public class location {
     private String name;
     private double latitude;
     private double longitude;
-    private String descripton;
-    private String county;
-    private String city;
+    private String country;
     private String address;
     private String postalcode;
 
@@ -16,4 +14,54 @@ public class location {
     public String getName(){
         return name;
     }
+     public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public String getPostalCode() {
+        return postalcode;
+    }
+
+    public String getContact() {
+        return country;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setPostalCode(String postalCode) {
+        this.postalcode = postalCode;
+    }
+    public void setcountry(String contact) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {      
+        return "Location{" +
+                "name='" + name + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", address='" + address + '\'' +
+                ", postalCode='" + postalcode + '\'' +
+                ", contact='" + country + '\'' +
+                '}';
+    }
+    
 }
+

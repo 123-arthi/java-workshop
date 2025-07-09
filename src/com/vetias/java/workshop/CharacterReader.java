7f4f7dd01403e68e0cac046743b559a1c5f9a749
arthxx-123 <arthxx1411@gmail.com>
2025-07-09 10:03:28 +0530

    AgeCalculator.java

---
+++ src/com/vetias/java/workshop/CharacterReader.java
@@ -0,0 +1,11 @@
+package workshop;
+
+import java.io.BufferedReader;
+import java.io.Reader;
+
+public class CharacterReader {
+
+    public static void main(String[] args) {
+        try(BufferedReader reader=newBuffer Reader(new FileReader(file name:"READER.MD")));
+    }
+}

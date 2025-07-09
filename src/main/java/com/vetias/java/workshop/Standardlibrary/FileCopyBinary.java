572168800b0813c1c5760fa54693b7d699e471c1
arthxx-123 <arthxx1411@gmail.com>
2025-07-09 14:48:30 +0530

    Maven

---
+++ src/main/java/com/vetias/java/workshop/Standardlibrary/FileCopyBinary.java
@@ -0,0 +1,27 @@
+package workshop.Standardlibrary;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopyBinary {
+    public static void main(String[] args) {
+        
+    try (FileInputStream inputStream = new FileInputStream("vijay.jpeg");
+    FileOutputStream OutputStream= new FileOutputStream("arthi.jpeg"))
+    {
+        byte[] chunk = new byte[1000];
+        int byteRead;
+        while ((byteRead=inputStream.read(chunk))!=-1){
+            OutputStream.write(chunk,0,byteRead);
+        }
+    } catch (IOException e) {
+        System.out.println("Error in reading or writting the file");
+    }
+    }
+}
+    
+        
+    
+

0d50789699cb6478852ca5e29f8a5f90b91984eb
arthxx-123 <arthxx1411@gmail.com>
2025-07-10 15:33:06 +0530

    LinkedHashSet.java

--- src/main/java/com/vetias/java/workshop/collections/StudentSet.java
+++ src/main/java/com/vetias/java/workshop/collections/StudentSet.java
@@ -2,12 +2,13 @@ package com.vetias.java.workshop.collections;
 
 import java.util.Comparator;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class StudentSet {
     public static void main(String[] args) {
-        Set<Integer> rollNos = new TreeSet<>(Comparator.reverseOrder());
+        Set<Integer> rollNos = new LinkedHashSet<>();
         rollNos.add(1001);   
         rollNos.add(1101); 
         rollNos.add(1011); 

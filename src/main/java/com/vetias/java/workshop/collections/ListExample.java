5240efff5353f70195f8d20a110f812b5b04d24a
arthxx-123 <arthxx1411@gmail.com>
2025-07-10 14:39:17 +0530

    NaturalListExample.java

--- src/main/java/com/vetias/java/workshop/collections/ListExample.java
+++ src/main/java/com/vetias/java/workshop/collections/ListExample.java
@@ -1,6 +1,7 @@
 package com.vetias.java.workshop.collections;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ListExample {
@@ -17,6 +18,7 @@ public class ListExample {
         for(int i=0;i<names.size();i++){
             System.out.println(names.get(i));
         }
+        names.sort(Comparator.naturalOrder());
 
 
     }
